b6f48a48364fcbe38c69b5097f5367d003e4c5d5
Nithin S V <nithinsurendran321@gamil.com>
2020-09-16 20:42:44 +0530

    Git Automation

--- auto.sh
+++ auto.sh
@@ -8,15 +8,15 @@ read cques
 if [ $cques == "y" ]
 then
 	echo "Enter the message"
-	 read message
-	 git commit --m "$message"
-fi
-echo "Do you want to push?(y/n) "
-read ques
-if [ $ques == "y" ]
-then
-    git remote --v
-    echo "Enter the repository name:"
-    read repo
-	git push $repo master
+	read message
+	git commit --m "$message"
+	echo "Do you want to push?(y/n) "
+	read ques
+	if [ $ques == "y" ]
+	then
+    	git remote --v
+    	echo "Enter the repository name:"
+    	read repo
+		git push $repo master
+	fi
 fi
